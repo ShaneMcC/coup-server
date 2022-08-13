c18391ca00c7176a11b532991bf6c5e156d9c8ee
Shane Mc Cormack <shanemcc@gmail.com>
2022-08-13 05:26:06 +0100

    Allow (Counter)Actions to have no valid cards. Not sure why, but allow it anyway.

--- src/Game/GameStates/PlayerChallengedTurnState.js
+++ src/Game/GameStates/PlayerChallengedTurnState.js
@@ -88,7 +88,7 @@ export default class PlayerChallengedTurnState extends GameState {
                 return [false, 'Player can not reveal influence they do not have.'];
             }
 
-            if (challengedAction.validCards.indexOf(target) > -1) {
+            if (challengedAction.validCards?.indexOf(target) > -1) {
                 this.game.emit('playerPassedChallenge', { 'player': this.player.id, 'influence': target });
                 this.game.emit('playerMustDiscardInfluence', { 'player': this.challenger.id, 'reason': 'Player unsuccessfully challenged' });
             } else {
