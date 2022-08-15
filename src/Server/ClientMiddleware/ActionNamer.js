2fb2b1d52a8f2e35a56d0252a36daa3414df1ed4
Shane Mc Cormack <shanemcc@gmail.com>
2022-08-15 01:11:33 +0100

    Show names for counteractions in events.

--- src/Server/ClientMiddleware/ActionNamer.js
+++ src/Server/ClientMiddleware/ActionNamer.js
@@ -19,9 +19,14 @@ export default class ActionNamer extends ClientMiddleware {
         if (!this.enabled()) { return; }
         var thisGame = this.#server.getGame(event.game);
 
-        if (event.action && !event.actionName && thisGame.GameActions[event.action]) {
-            event.actionName = thisGame.GameActions[event.action].name;
+        if (event.action && !event.actionName) {
+            if (thisGame.GameActions[event.action]) {
+                event.actionName = thisGame.GameActions[event.action].name;
+            } else if (thisGame.GameCounterActions[event.action]) {
+                event.actionName = thisGame.GameCounterActions[event.action].name;
+            }
         }
+
         if (event.counter && !event.counterName && thisGame.GameCounterActions[event.counter]) {
             event.counterName = thisGame.GameCounterActions[event.counter].name;
         }
